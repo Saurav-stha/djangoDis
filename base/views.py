d5f61c72ead697a5e01b329f8e9748976313d839
saurav <shresthsaurav8@gmail.com>
2024-04-22 23:02:50 +0545

    feat: basic setup

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+servers = [
+    {'id' : 1 , 'name' : 'Servers Server'},
+    {'id' : 2 , 'name' : 'Anime'},
+    {'id' : 3 , 'name' : 'Music'},
+    
+]
+
+def home(request):
+    context = {'servers' :servers}
+    return render(request, 'base/home.html' , context)
+
+def server(request):
+    return render(request, 'base/server.html')
