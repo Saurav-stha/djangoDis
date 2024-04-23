db75f15cb9d37985138ec11e271be0f96d910923
saurav <shresthsaurav8@gmail.com>
2024-04-23 15:23:54 +0545

    feat: add topic and msg - insert data manually in db - display more info for each server

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=220)
+
+    def __str__(self):
+        return self.name
+
+class Server(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True , blank=True) # null for db and blank for frontend
+    # members = 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Msg(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField()
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:40]
+    
--- base/views.py
+++ base/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
+from .models import Server
 
 # Create your views here.
 
-servers = [
-    {'id' : 1 , 'name' : 'Servers Server'},
-    {'id' : 2 , 'name' : 'Anime'},
-    {'id' : 3 , 'name' : 'Music'},
+# servers = [
+#     {'id' : 1 , 'name' : 'Servers Server'},
+#     {'id' : 2 , 'name' : 'Anime'},
+#     {'id' : 3 , 'name' : 'Music'},
     
-]
+# ]
 
 def home(request):
+    servers = Server.objects.all()
     context = {'servers' :servers}
     return render(request, 'base/home.html' , context)
 
 def server(request,sid):
-    for i in servers:
-        if i['id'] == int(sid):
-            server = i
+    # for i in servers:
+    #     if i['id'] == int(sid):
+    #         server = i
+    server = Server.objects.get(id=sid)
 
     context = {'server': server}
     return render(request, 'base/server.html' , context)
